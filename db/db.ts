72a92dc153790c70a603ae1ed42b27e970844a7d
lideming <me@yuuza.net>
2023-02-05 09:30:14 +0800

    update db

--- db/db.ts
+++ db/db.ts
@@ -4,106 +4,105 @@ import { mkdir } from "fs/promises";
 import { addCollection, getNotesByOwner, updateNote } from "./notes";
 
 // Somehow Next.js inlined this file multiple times, ensure there is only one instance here.
-export const { db, initDb } = (globalThis._mydb as null || (globalThis._mydb = {
-    db: new Database(),
-    initDb: (async function () {
-        console.info("Initialing DB...");
-        await mkdir("./data/", { recursive: true });
-        await db.openFile("./data/db.btrdb")
+export const { db, initDb } = globalThis._mydb as null ||
+  (globalThis._mydb = createDatabase());
 
-        const notes = await db.createSet<Note>("notes", "doc");
-        notes.idGenerator = nanoIdGenerator(10);
-        await notes.useIndexes({
-            owner: x => x.owner,
-            col: x => x.col,
-        });
+function createDatabase() {
+  const db = new Database();
+  const initDb = async function () {
+    console.info("Initialing DB...");
+    await mkdir("./data/", { recursive: true });
+    await db.openFile("./data/db.btrdb");
 
-        const users = await db.createSet<UserInfo>("users", "doc");
-        await users.useIndexes({
-            email: x => x.email,
-        });
+    const notes = await db.createSet<Note>("notes", "doc");
+    notes.idGenerator = nanoIdGenerator(10);
+    await notes.useIndexes({
+      owner: (x) => x.owner,
+      col: (x) => x.col,
+    });
 
-        const cols = await db.createSet<NoteCollection>("notecols", "doc");
-        cols.idGenerator = nanoIdGenerator(10);
-        await cols.useIndexes({
-            owner: x => x.owner,
-        });
+    const users = await db.createSet<UserInfo>("users", "doc");
+    await users.useIndexes({
+      email: (x) => x.email,
+    });
 
-        await migrate({ notes, users, cols });
+    const cols = await db.createSet<NoteCollection>("notecols", "doc");
+    cols.idGenerator = nanoIdGenerator(10);
+    await cols.useIndexes({
+      owner: (x) => x.owner,
+    });
 
-        await db.commit();
-        // console.info("Rebuilding DB...");
-        // await db.rebuild();
-        console.info("Finished initialing DB.");
-    })(),
-}));
+    await migrate({ notes, users, cols });
+
+    await db.commit();
+    // console.info("Rebuilding DB...");
+    // await db.rebuild();
+    console.info("Finished initialing DB.");
+
+    return { notes, users, cols };
+  };
+  return { db, initDb: initDb() };
+}
 
 export async function getNotesSet() {
-    await initDb;
-    const set = await db.getSet<Note>("notes", "doc");
-    set.idGenerator = nanoIdGenerator(10);
-    return set;
+  return (await initDb).notes;
 }
 
 export async function getNoteCollectionsSet() {
-    await initDb;
-    const set = await db.getSet<NoteCollection>("notecols", "doc");
-    set.idGenerator = nanoIdGenerator(10);
-    return set;
+  return (await initDb).cols;
 }
 
 export async function getUsersSet() {
-    await initDb;
-    return await db.getSet<UserInfo>("users", "doc");
+  return (await initDb).users;
 }
 
 async function migrate(sets: {
-    users: IDbDocSet<UserInfo>,
-    notes: IDbDocSet<Note>,
-    cols: IDbDocSet<NoteCollection>,
+  users: IDbDocSet<UserInfo>;
+  notes: IDbDocSet<Note>;
+  cols: IDbDocSet<NoteCollection>;
 }) {
-    const { users, notes, cols } = sets;
-    const config = await db.createSet("config", "kv");
-    const origVer = await config.get("version");
-    let ver = origVer;
-    console.info("DB version", ver);
-    if (ver == null) {
-        ver = 1;
+  const { users, notes, cols } = sets;
+  const config = await db.createSet("config", "kv");
+  const origVer = await config.get("version");
+  let ver = origVer;
+  console.info("DB version", ver);
+  if (ver == null) {
+    ver = 1;
+  }
+  if (ver == 1) {
+    ver = 2;
+    // convert number id to nanoid
+    const now = Date.now();
+    for (const note of await notes.getAll()) {
+      if (typeof note.id == "number") {
+        await notes.delete(note.id);
+        note.id = null;
+        note.mtime = note.ctime = now;
+        await notes.insert(note);
+      }
     }
-    if (ver == 1) {
-        ver = 2;
-        // convert number id to nanoid
-        const now = Date.now();
-        for (const note of await notes.getAll()) {
-            if (typeof note.id == "number") {
-                await notes.delete(note.id);
-                note.id = null;
-                note.mtime = note.ctime = now;
-                await notes.insert(note);
-            }
-        }
+  }
+  if (ver == 2) {
+    ver = 3;
+    // make a default collection for each user
+    const now = Date.now();
+    for (const uid of await users.getIds()) {
+      const col: NoteCollection = {
+        id: null,
+        name: "default",
+        owner: uid,
+        ctime: now,
+        mtime: now,
+      };
+      await cols.insert(col);
+      for (const note of await notes.findIndex("owner", uid)) {
+        note.col = col.id;
+        await notes.update(note);
+      }
     }
-    if (ver == 2) {
-        ver = 3;
-        // make a default collection for each user
-        const now = Date.now();
-        for (const uid of await users.getIds()) {
-            const col: NoteCollection = {
-                id: null,
-                name: "default",
-                owner: uid,
-                ctime: now,
-                mtime: now,
-            };
-            await cols.insert(col);
-            for (const note of await notes.findIndex("owner", uid)) {
-                note.col = col.id;
-                await notes.update(note);
-            }
-        }
-    }
-    if (ver !== origVer) {
-        console.info("DB version changed", ver);
-        await config.set("version", ver);
-    }
+  }
+  if (ver !== origVer) {
+    console.info("DB version changed", ver);
+    await config.set("version", ver);
+  }
 }
